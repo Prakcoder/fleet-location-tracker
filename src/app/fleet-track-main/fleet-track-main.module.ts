423fe6039bb2b40dfbade036ce20b88f29f5b8d1
Prakcoder <Prakashverma042@gmail.com>
2019-04-27 12:31:03 +0530

    Added vehicle list component

--- src/app/fleet-track-main/fleet-track-main.module.ts
+++ src/app/fleet-track-main/fleet-track-main.module.ts
@@ -2,11 +2,15 @@ import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
+import { VehicleListComponent } from '../components/vehicle-list/vehicle-list.component';
 import { ApiService } from '../services/api.service';
 import { FleetTrackMainComponent } from './fleet-track-main.component';
 
 @NgModule({
-    declarations: [FleetTrackMainComponent],
+    declarations: [
+        FleetTrackMainComponent,
+        VehicleListComponent
+    ],
     imports: [
         CommonModule,
         HttpClientModule

6f476e82232132607cf4adca265e0e291a4b5a43
Prakcoder <Prakashverma042@gmail.com>
2019-04-27 11:10:37 +0530

    Added api service

--- src/app/fleet-track-main/fleet-track-main.module.ts
+++ src/app/fleet-track-main/fleet-track-main.module.ts
@@ -1,12 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { ApiService } from '../services/api.service';
 import { FleetTrackMainComponent } from './fleet-track-main.component';
 
 @NgModule({
     declarations: [FleetTrackMainComponent],
     imports: [
         CommonModule
-    ]
+    ],
+    providers: [ApiService]
 })
 export class FleetTrackMainModule { }

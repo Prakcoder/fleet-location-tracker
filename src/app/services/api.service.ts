dea6ac6d1d1b7f2fd88f93f58296e87fd6132563
Prakcoder <Prakashverma042@gmail.com>
2019-04-27 12:27:53 +0530

    Using types for api response

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { of } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
 
 import { MOCK_DATA } from '../constants/data';
@@ -15,8 +15,8 @@ export class ApiService {
 
     public loadVehicle() {
         const url = this.baseUrl + 'analytics/live/new/eJwFwYENACAIA7CLlohghHNExhnebiuij6nNio1pHTC9RA4uuC$J4xFV$OTpCzE=';
-        return this.http.get(url).pipe(
-            map(response => (response as any).vehicles),
+        return this.http.get<ApiResponse>(url).pipe(
+            map(response => response.vehicles),
             catchError(error => {
                 // Request is failing due to Access-Control-Allow-Origin set 'https://www.fleetx.io' orgin only in response
                 // So I am using mock data
@@ -25,7 +25,11 @@ export class ApiService {
         );
     }
 
-    private getMockData() {
+    private getMockData(): Observable<Vehicle[]> {
         return of(MOCK_DATA.vehicles);
     }
+}
+
+interface ApiResponse {
+    vehicles: Vehicle[];
 }

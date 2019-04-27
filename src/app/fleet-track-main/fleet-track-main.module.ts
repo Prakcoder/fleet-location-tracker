d660678ac8126dcdf9b4d1927439c2c43caecb06
Prakcoder <Prakashverma042@gmail.com>
2019-04-27 16:15:32 +0530

    Showing loader and fix some styles

--- src/app/fleet-track-main/fleet-track-main.module.ts
+++ src/app/fleet-track-main/fleet-track-main.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
+import { LoaderComponent } from '../components/loader/loader.component';
 import { VehicleListComponent } from '../components/vehicle-list/vehicle-list.component';
 import { ApiService } from '../services/api.service';
 import { FleetTrackMainComponent } from './fleet-track-main.component';
@@ -9,7 +10,8 @@ import { FleetTrackMainComponent } from './fleet-track-main.component';
 @NgModule({
     declarations: [
         FleetTrackMainComponent,
-        VehicleListComponent
+        VehicleListComponent,
+        LoaderComponent
     ],
     imports: [
         CommonModule,
